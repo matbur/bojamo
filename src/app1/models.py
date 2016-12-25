2ddadbbbf7dc6c6913d927352fd702fc13d7c527
slamdunkk <218355@student.pwr.edu.pl>
2016-12-25 23:14:52 +0100

    I've made changes of DB models, now they are the same as in DB_SCHEME

--- src/app1/models.py
+++ src/app1/models.py
@@ -10,70 +10,78 @@ class User(models.Model):
     email = models.EmailField(max_length=40)
     password = models.CharField(max_length=40)
     permission = models.PositiveSmallIntegerField()
-
+    url = models.CharField(max_length=80)
+    description = models.TextField()
+    activated = models.BooleanField()
 
 class Group(models.Model):
     owner = models.IntegerField()
     name = models.CharField(max_length=50)
     date = models.DateField()
-
+    description = models.TextField()
 
 class UserGroup(models.Model):
     user = models.ForeignKey(User)
     group = models.ForeignKey(Group)
-
+    permission = models.PositiveSmallIntegerField()
 
 class Project(models.Model):
     group = models.ForeignKey(Group)
     name = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateField()
-
+    repository = models.CharField(max_length=80)
+    url = models.CharField(max_length=80)
 
 class UserProject(models.Model):
     user = models.ForeignKey(User)
     project = models.ForeignKey(Project)
-    permissions = models.IntegerField()
-
+    permission = models.PositiveSmallIntegerField()
 
 class Message(models.Model):
     content = models.TextField()
-
+    date = models.DateField()
 
 class UserMessage(models.Model):
     user = models.ForeignKey(User)
     message = models.ForeignKey(Message)
 
+class Status(models.Model):
+    name = models.CharField(max_length=20)
+
+class Priority(models.Model):
+    name = models.CharField(max_length=20)
 
 class Task(models.Model):
     project = models.ForeignKey(Project)
     name = models.CharField(max_length=50)
-
+    time = models.PositiveIntegerField()
+    description = models.TextField()
+    status = models.ForeignKey(Status)
+    reporter = models.ForeignKey(User)
+    priority = models.ForeignKey(Priority)
 
 class UserTask(models.Model):
     user = models.ForeignKey(User)
     task = models.ForeignKey(Task)
-
 
 class Comment(models.Model):
     task = models.ForeignKey(Task)
     content = models.TextField()
     date = models.DateField()
 
-
 class UserComment(models.Model):
     user = models.ForeignKey(User)
     comment = models.ForeignKey(Comment)
-
 
 class Sprint(models.Model):
     project = models.ForeignKey(Project)
     begin = models.DateField()
     end = models.DateField()
     number = models.IntegerField()
-
+    status = models.BooleanField()
 
 class SprintTask(models.Model):
-    spring = models.ForeignKey(Sprint)
+    sprint = models.ForeignKey(Sprint)
     task = models.ForeignKey(Task)
     time = models.IntegerField()
