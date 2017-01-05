167e1aa596a93f51803600c5b12c2e9a03a99855
slamdunkk <218355@student.pwr.edu.pl>
2017-01-05 12:46:47 +0100

    I added form for Task creation!

---
+++ src/task/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = {'name', 'status', 'priority', 'time', 'description'}
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'status': forms.Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'priority': forms.Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'time': forms.NumberInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control'
+                }
+            )
+        }
