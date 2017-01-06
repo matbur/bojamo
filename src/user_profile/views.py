d34f5a3212b3c51bfdc0f88426476fe268944c5e
matbur95 <mateusz@burniak.pl>
2017-01-06 10:52:53 +0100

    In user_profile/views use render instead of render_to_response, simplify user_detail function

--- src/user_profile/views.py
+++ src/user_profile/views.py
@@ -1,15 +1,13 @@
-from django.contrib import auth, messages
-from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.contrib.messages import get_messages
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.template.context_processors import csrf
-from django.utils.http import is_safe_url
+
 from group.models import UserGroup
 from project.models import UserProject
-from django.views.generic import CreateView, ListView, DetailView
 
 
 @login_required
@@ -18,22 +16,19 @@ def user_detail(request, username):
 
     user = get_object_or_404(User, username=username)
     context['get_user'] = user
-    userGroups = UserGroup.objects.filter(user=user)
+    user_groups = UserGroup.objects.filter(user=user)
     context['groupProjects'] = {}
-    for userGroup in userGroups:
-        projects = UserProject.objects.filter(user=user, project__group=userGroup.group)
-        if projects:
-            context['groupProjects'].update({userGroup.group.name: projects})
-        else:
-            context['groupProjects'].update({userGroup.group.name:  None})
-    return render_to_response(
+    for user_group in user_groups:
+        projects = UserProject.objects.filter(user=user, project__group=user_group.group)
+        context['groupProjects'][user_group.group.name] = projects or None
+    return render(
+        request,
         'user_profile/user_detail.html',
         context
     )
 
 
 def login(request):
-
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse_lazy('loggedin'))
 
@@ -46,7 +41,7 @@ def login(request):
         password = request.POST.get('password', None)
         user = auth.authenticate(username=username, password=password)
 
-        if user != None:
+        if user is not None:
             if user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse_lazy('loggedin'))
@@ -56,14 +51,15 @@ def login(request):
             context['messages'].append('Username or password is invalid!')
 
     context.update(csrf(request))
-    return render_to_response(
+    return render(
+        request,
         'user_profile/login.html',
         context
     )
 
 
 def registration(request):
-    ###TODO filtr fields if they contain default values!!!
+    # TODO filter fields if they contain default values!!!
 
     context = {
         'messages': []
@@ -81,40 +77,44 @@ def registration(request):
         user = User.objects.filter(username=form['username'])
         email = User.objects.filter(email=form['email'])
 
-
         context.update(csrf(request))
         for key, value in form.items():
             if value is None:
                 context['messages'].append('Please fill all fields')
                 context.update(csrf(request))
-                return render_to_response(
+                return render(
+                    request,
                     'user_profile/registration.html',
                     context
                 )
 
         if user:
-            context['messages'].append('This username already exitsts!')
+            context['messages'].append('This username already exists!')
         elif email:
             context['messages'].append('This email is in use!')
         elif form['password'] != form['confirm_password']:
             context['messages'].append('Password is invalid!')
         else:
-            user = User.objects.create_user(first_name=form['first_name'],
-                           last_name=form['last_name'],
-                           email=form['email'],
-                           username=form['username'],
-                           password=form['password']
-                           )
+            user = User.objects.create_user(
+                first_name=form['first_name'],
+                last_name=form['last_name'],
+                email=form['email'],
+                username=form['username'],
+                password=form['password']
+            )
             auth.login(request, user)
             return HttpResponseRedirect(reverse_lazy('loggedin'))
     context.update(csrf(request))
-    return render_to_response(
+    return render(
+        request,
         'user_profile/registration.html',
         context
     )
 
+
 @login_required
 def loggedin(request):
-    return render_to_response(
+    return render(
+        request,
         'user_profile/loggedin.html'
     )
