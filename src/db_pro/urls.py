14abc23fe1671e5a6b6d83317040bcf1800a478f
matbur95 <mateusz@burniak.pl>
2017-01-08 14:43:44 +0100

    Add proper title block in templates

--- src/db_pro/urls.py
+++ src/db_pro/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
-from .views import IndexView
 
-from user_profile import urls as user_profile_urls
 from group import urls as group_urls
 from project import urls as project_urls
-
+from user_profile import urls as user_profile_urls
+from .views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
